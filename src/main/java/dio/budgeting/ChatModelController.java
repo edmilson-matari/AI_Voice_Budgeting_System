e6e928fc2c89232f62dbff2438470c912e51cbba
Edmilson Matari <mitcho@mitcho.mitch>
2026-05-25 06:54:30 -0700

    first commit

---
+++ src/main/java/dio/budgeting/ChatModelController.java
@@ -0,0 +1,29 @@
+package dio.budgeting;
+
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api")
+public class ChatModelController {
+
+    private final OpenAiChatModel openAiChatModel;
+    public ChatModelController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+    }
+    @GetMapping("/chat-model")
+    public String chat(@RequestParam String prompt) {
+        return this.openAiChatModel.call(prompt);
+    }
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+    
+}
